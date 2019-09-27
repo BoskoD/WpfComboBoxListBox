051d894011e2825e8af9908568611ef3f597389f
barjunkey.gmail.com <barjunkey@gmail.com>
2019-09-27 20:59:55 +0200

    Add project files.

---
+++ WpfComboBoxListBox_WPF1/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfComboBoxListBox_WPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBox1.SelectedIndex > -1)
+            {
+                ComboBoxItem selektovano = ComboBox1.SelectedItem as ComboBoxItem;
+                TextBlock1.Text = selektovano.Content.ToString();
+            }
+            else
+            {
+                ComboBox1.Text = "";
+                TextBlock1.Text = "";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBox1.SelectedIndex = -1;
+          
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (!string.IsNullOrEmpty(TextBox1.Text))
+                {
+                    ComboBoxItem cbi = new ComboBoxItem();
+                    cbi.Content = TextBox1.Text;
+                    ComboBox1.Items.Add(cbi);
+                    TextBox1.Clear();
+                    ComboBox1.SelectedIndex = ComboBox1.Items.Count - 1;
+                }
+            }
+        }
+    }
+}
---
+++ WpfComboBoxListBox_WPF1/Window1.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfComboBoxListBox_WPF1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        Dictionary<string, int> recnik = new Dictionary<string, int>();
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // povezivanje int
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    ComboBox1.Items.Add(i);
+            //} 
+
+            /////////////////////////////////////////////////////////////////
+
+            // povezivanje array
+            //string[] stavke = {"stavka1", "stavka2", "stavka3", "stavka4" };
+            //ComboBox1.ItemsSource = stavke;
+
+
+            /////////////////////////////////////////////////////////////////
+
+            recnik.Add("Domaca kafa", 10);
+            recnik.Add("Rakija", 20);
+            recnik.Add("Vino", 40);
+            recnik.Add("Limunada", 120);
+            ComboBox1.ItemsSource = recnik;
+            ComboBox1.DisplayMemberPath = "Key";
+
+
+
+
+
+        }
+
+
+        private void ComboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // povezivanje int
+            //int a = (int)ComboBox1.SelectedItem;
+            //TextBlock1.Text = a.ToString();
+
+            /////////////////////////////////////////////////////////////////
+
+            // povezivanje array
+            //string selektovano = ComboBox1.SelectedItem.ToString();
+            //TextBlock1.Text = $"{selektovano}";
+
+            /////////////////////////////////////////////////////////////////
+
+            KeyValuePair<string, int> selektovano = (KeyValuePair<string, int>)ComboBox1.SelectedItem;
+            TextBlock1.Text = $"Cena je: {selektovano.Value}";
+
+
+
+
+        }
+    }
+}
